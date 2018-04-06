047c75d5198c44fc4aa9bf619864322ff3507c3e
Ihor Horash <gorashigor@gmail.com>
2018-04-06 21:56:25 +0300

    Identity server front-end customized

--- IdentityApp/Models/AccountViewModels/LoginViewModel.cs
+++ IdentityApp/Models/AccountViewModels/LoginViewModel.cs
@@ -14,9 +14,10 @@ namespace IdentityApp.Models.AccountViewModels
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Запам'ятати мене?")]
         public bool RememberMe { get; set; }
     }
 }
